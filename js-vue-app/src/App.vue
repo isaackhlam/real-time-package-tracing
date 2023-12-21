946ed304508c4992d75b681b5fd33d694ec04ba5
CommitSudoku <97761927+isaackhlam@users.noreply.github.com>
2023-12-21 18:39:51 +0800

    feat(frontend): connect frontend to backend to login (#5)
    * feat: add form rule for login and register user
    
    * feat: install graphql request package to query server
    
    * feat: connect frontend app to server to login and register
    
    * feat: install cookies to save login information
    
    * feat: save login information in Cookies
    
    * feat: nav bar change depends on login state and add logout button
    
    * feat: add pop out dialog for user feedback
    
    * style: prettier

--- js-vue-app/src/App.vue
+++ js-vue-app/src/App.vue
@@ -1,11 +1,14 @@
 <script setup>
   import { RouterView } from 'vue-router';
+  import { NMessageProvider } from 'naive-ui';
 </script>
 
 <template>
-  <component :is="$route.meta.layout || 'div'">
-    <RouterView />
-  </component>
+  <n-message-provider>
+    <component :is="$route.meta.layout || 'div'">
+      <RouterView />
+    </component>
+  </n-message-provider>
 </template>
 
 <style>
--- js-vue-app/src/components/Login.vue
+++ js-vue-app/src/components/Login.vue
@@ -1,5 +1,151 @@
 <script setup>
-import { NCard, NTabs, NTabPane, NForm, NFormItemRow, NInput, NButton } from 'naive-ui';
+  import { ref } from 'vue';
+  import { NCard, NTabs, NTabPane, NForm, NFormItemRow, NInput, NButton, useMessage } from 'naive-ui';
+  import { request, gql } from 'graphql-request';
+  import Cookies from 'js-cookie';
+
+  const message = useMessage();
+
+  // Ref
+  const formRef = ref(null);
+  const loginModelRef = ref({
+    userId: null,
+    password: null,
+  });
+  const registerModelRef = ref({
+    userId: null,
+    username: null,
+    password: null,
+    reenteredPassword: null,
+  });
+  const reenteredPasswordItemRef = ref(null);
+
+  // Validator and Form Rule
+  const validatePasswordSame = (_rule, value) =>
+    value === registerModelRef.value.password;
+
+  const RegisterRules = {
+    userId: [
+      {
+        required: true,
+        message: 'User ID is required'
+      }
+    ],
+    username: [
+      {
+        required: false,
+      }
+    ],
+    password: [
+      {
+        required: true,
+        message: 'Password is required'
+      }
+    ],
+    reenteredPassword: [
+      {
+        required: true,
+        message: 'Re-entered password is required',
+        trigger: ['input', 'blur']
+      },
+      {
+        validator: validatePasswordSame,
+        message: 'Password is not same as re-entered password',
+        trigger: ['input', 'blur', 'password-input']
+      }
+    ]
+  };
+
+  const LoginRules = {
+    userId: [
+      {
+        required: true,
+        message: 'Username is required'
+      }
+    ],
+    password: [
+      {
+        required: true,
+        message: 'Password is required'
+      }
+    ]
+  };
+
+  // Button and Input Handler
+  const handlePasswordInput = () => {
+    if (registerModelRef.value.reenteredPassword) {
+      reenteredPasswordItemRef.value.validate({ trigger: 'password-input' });
+    }
+  };
+
+  const handleRegisterButtonClick = (e) => {
+    e.preventDefault();
+    formRef.value?.validate(async (errors) => {
+      if (!errors) {
+        const registerMutation = gql`
+          mutation($id: ID!, $name: String, $password: String!) {
+            register(input: {id: $id, name: $name, password: $password}) {
+              id
+            }
+          }
+        `;
+        const variables = {
+          id: registerModelRef.value.userId,
+          name: registerModelRef.value.username || "UserName",
+          password: registerModelRef.value.password,
+        };
+        try {
+          const data = await request("http://localhost:4000/graphql", registerMutation, variables);
+          console.log(data);
+        } catch (error) {
+          console.log("Server Error: ", error);
+          message.error(error.response.errors[0].message);
+        }
+      } else {
+        console.log(errors);
+      }
+    });
+  };
+
+  const handleLoginButtonClick = () => {
+    formRef.value?.validate(async (errors) => {
+      if (!errors) {
+        const loginMutation = gql`
+          mutation($id: ID!, $password: String!) {
+            login(input: {id: $id, password: $password}) {
+              token
+            }
+          }
+        `;
+        const meQuery = gql`
+          query {
+            me {
+              id
+              name
+            }
+          }
+        `;
+        const variables = {
+          id: loginModelRef.value.userId,
+          password: loginModelRef.value.password,
+        };
+        try {
+          const data = await request("http://localhost:4000/graphql", loginMutation, variables);
+          const me = await request("http://localhost:4000/graphql", meQuery, {}, { "x-token": data.login.token});
+          Cookies.set('x-token', data.login.token);
+          Cookies.set('userId', me.me.id);
+          Cookies.set('username', me.me.name);
+          message.success("Login Successfully");
+        } catch (error) {
+          console.log("Server Error: ", error);
+          message.error(error.response.errors[0].message);
+        }
+      } else {
+        console.log("Form Input Error", errors);
+      }
+    });
+  };
+
 </script>
 
 <template>
@@ -13,31 +159,52 @@ import { NCard, NTabs, NTabPane, NForm, NFormItemRow, NInput, NButton } from 'na
       pane-style="padding-left: 4px; padding-right: 4px; box-sizing: border-box;"
     >
       <n-tab-pane name="signin" tab="Sign in">
-        <n-form>
-          <n-form-item-row label="Username">
-            <n-input />
+        <n-form ref="formRef" :model="loginModelRef" :rules="LoginRules">
+          <n-form-item-row label="UserId" path="userId">
+            <n-input v-model:value="loginModelRef.userId" @keydown.enter.prevent />
           </n-form-item-row>
-          <n-form-item-row label="Password">
-            <n-input />
+          <n-form-item-row label="Password" path="password">
+            <n-input v-model:value="loginModelRef.password" type="password" @keydown.enter.prevent />
           </n-form-item-row>
         </n-form>
-        <n-button type="primary" block secondary strong>
+        <n-button
+          type="primary"
+          block
+          secondary
+          strong
+          :disabled="loginModelRef.userId === null"
+          @click="handleLoginButtonClick"
+        >
           Sign In
         </n-button>
       </n-tab-pane>
       <n-tab-pane name="signup" tab="Sign up">
-        <n-form>
-          <n-form-item-row label="Username">
-            <n-input />
+        <n-form ref="formRef" :model="registerModelRef" :rules="RegisterRules">
+          <n-form-item-row label="UserId" path="userId">
+            <n-input v-model:value="registerModelRef.userId" @keydown.enter.prevent />
           </n-form-item-row>
-          <n-form-item-row label="Password">
-            <n-input />
+          <n-form-item-row label="UserName" path="username">
+            <n-input v-model:value="registerModelRef.username" @keydown.enter.prevent />
           </n-form-item-row>
-          <n-form-item-row label="Reenter Password">
-            <n-input />
+          <n-form-item-row label="Password" path="password">
+            <n-input
+              v-model:value="registerModelRef.password"
+              type="password"
+              @input="handlePasswordInput"
+              @keydown.enter.prevent/>
+          </n-form-item-row>
+          <n-form-item-row label="Reenter Password" path="reenteredPassword" ref="reenteredPasswordItemRef">
+            <n-input v-model:value="registerModelRef.reenteredPassword" type="password" @keydown.enter.prevent />
           </n-form-item-row>
         </n-form>
-        <n-button type="primary" block secondary strong>
+        <n-button
+          type="primary"
+          block
+          secondary
+          strong
+          :disabled="registerModelRef.userId === null"
+          @click="handleRegisterButtonClick"
+        >
           Sign up
         </n-button>
       </n-tab-pane>
