309cce47c7b98a9608178b3e4722b7b88c412a48
isaackhlam <97761927+isaackhlam@users.noreply.github.com>
2023-12-22 03:56:21 +0800

    wip: wip

--- js-vue-app/src/components/Map.vue
+++ js-vue-app/src/components/Map.vue
@@ -1,21 +1,26 @@
 <script setup>
-  import { Map, NavigationControl } from 'maplibre-gl';
-  import { shallowRef, onMounted, onUnmounted, markRaw } from 'vue';
+  import { Map, NavigationControl, Marker } from 'maplibre-gl';
+  import { shallowRef, onMounted, onUnmounted, markRaw, ref } from 'vue';
+  import { NInput, NButton, useMessage, NInputGroup } from 'naive-ui';
+  import { request, gql } from 'graphql-request';
 
-  const mapContainer = shallowRef(null);
-  const map = shallowRef(null);
+  const message = useMessage();
+
+  const packageIdRef = ref(null);
+  const mapContainer = ref(null);
+  const map = ref(null);
 
   onMounted(() => {
     const apiKey = import.meta.env.VITE_MAPTILER_API_KEY;
 
-    const initialState = { lng: 16.6266, lat: 49.2125, zoom: 14 };
+    const initialState = { lng: 114.205634, lat: 22.422129, zoom: 14 };
 
-    map.value = markRaw(new Map({
+    map.value = new Map({
       container: mapContainer.value,
       style: `https://api.maptiler.com/maps/streets/style.json?key=${apiKey}`,
       center: [initialState.lng, initialState.lat],
       zoom: initialState.zoom
-    }));
+    });
 
     map.value.addControl(new NavigationControl(), 'top-right');
 
@@ -24,9 +29,46 @@
   onUnmounted(() => {
     map.value?.remove();
   })
+
+  const handleSearchButtonClick = async () => {
+    if (packageIdRef.value) {
+      const findPacakgeQuery = gql`
+        query($id: ID!) {
+          findPackage(input: {id: $id}){
+            location{
+              longitude
+              latitude
+            }
+            lastUpdateTime
+          }
+        }
+      `;
+      const variables = {id: packageIdRef.value};
+      try {
+        const data = await request("http://localhost:4000/graphql", findPacakgeQuery, variables);
+        console.log(data.findPackage);
+        const long = data.findPackage.location.longitude;
+        const lat = data.findPackage.location.latitude;
+        map.value.flyTo({ center: [long, lat], zoom: 20});
+        const marker = new Marker({color: "#FF0000"})
+          .setLngLat([Number(data.findPackage.location.longitude), Number(data.findPackage.location.latitude)])
+          .addTo(map.value);
+        message.success("Longitude: " + long + "\nLatitude: " + lat + "\nUpdated Time: " + data.findPackage.lastUpdateTime);
+      } catch(error) {
+        console.log("Server Error: ", error);
+        message.error(error.response.errors[0].message)
+      }
+    }
+  }
 </script>
 
 <template>
+    <n-input-group>
+      <n-input v-model:value="packageIdRef" :style="{ width: '90%' }" placeholder="Input package Id"/>
+      <n-button type="primary" @click="handleSearchButtonClick">
+        Search
+      </n-button>
+    </n-input-group>
   <div class="map-wrap">
     <div class="map" ref="mapContainer"></div>
   </div>
@@ -43,8 +85,8 @@
   }
 
   .map {
-    position: absolute;
     width: 100%;
     height: 100%;
+    z-index: -999;
   }
 </style>
