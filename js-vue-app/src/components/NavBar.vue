33fee110d5127e11c0ff3f34612c05df41c3a4fb
isaackhlam <isaac.lamkh@gmail.com>
2023-12-21 18:26:21 +0800

    feat: nav bar change depends on login state and add logout button

--- js-vue-app/src/components/NavBar.vue
+++ js-vue-app/src/components/NavBar.vue
@@ -1,67 +1,99 @@
 <script setup>
-  import { h } from 'vue';
+  import { h, ref } from 'vue';
   import { RouterLink } from 'vue-router';
-  import { NIcon, NMenu } from 'naive-ui';
+  import { NIcon, NMenu, NP } from 'naive-ui';
   import {
     MapOutline as MapIcon,
     PersonOutline as LoginIcon,
     HomeOutline as HomeIcon
   } from '@vicons/ionicons5';
+  import Cookies from 'js-cookie';
 
-const renderIcon = (icon) => {
-  return () => h(NIcon, null, { default: () => h(icon) })
-}
+  const meRef = ref(null);
+  meRef.value = Cookies.get('x-token');
 
-const menuOptions = [
-  {
-    label: () =>
-      h(
-        RouterLink,
-        {
-          to: {
-            name: 'home',
-          }
-        },
-        "Home"
-      ),
-    key: 'home',
-    icon: renderIcon(HomeIcon)
-  },
-  {
-    label: () =>
-      h(
-        RouterLink,
-        {
-          to: {
-            name: 'track',
-          }
-        },
-        "Track"
-      ),
-    key: 'track',
-    icon: renderIcon(MapIcon)
-  },
-  {
-    label: () =>
-      h(
-        RouterLink,
-        {
-          to: {
-            name: 'login',
+  const renderIcon = (icon) => {
+    return () => h(NIcon, null, { default: () => h(icon) })
+  }
+
+  const menuOptions = [
+    {
+      label: () =>
+        h(
+          RouterLink,
+          {
+            to: {
+              name: 'home',
+            }
           },
-        },
-        "Login"
-      ),
-    key: 'login',
-    icon: renderIcon(LoginIcon),
-  },
-]
+          "Home"
+        ),
+      key: 'home',
+      icon: renderIcon(HomeIcon)
+    },
+    {
+      label: () =>
+        h(
+          RouterLink,
+          {
+            to: {
+              name: 'track',
+            }
+          },
+          "Track"
+        ),
+      key: 'track',
+      icon: renderIcon(MapIcon)
+    },
+  ]
 
+  const loginOption = [
+    {
+      label: () =>
+        h(
+          RouterLink,
+          {
+            to: {
+              name: 'login',
+            },
+          },
+          "Login"
+        ),
+      key: 'login',
+      icon: renderIcon(LoginIcon),
+    },
+  ]
+
+  const logoutOption = [
+    {
+      label: () => h(NP, { onClick: () => handleLogoutClick() }, 'Log Out'),
+      key: 'logout',
+      icon: renderIcon(LoginIcon)
+    },
+  ];
+
+  const handleLogoutClick = () => {
+    Cookies.remove('x-token');
+    Cookies.remove('userId');
+    Cookies.remove('username');
+    meRef.value = undefined;
+  };
 </script>
 
 <template>
+  <div class="header-container">
     <n-menu :options="menuOptions" mode="horizontal" />
+    <n-menu v-if="meRef === undefined" :options="loginOption" mode="horizontal" class="login-button" />
+    <n-menu v-else :options="logoutOption" mode="horizontal" class="login-button" />
+  </div>
 </template>
 
 <style scoped>
+  .header-container {
+    display: flex;
+  }
+
+  .login-button {
+    justify-content: flex-end;
+  }
 </style>
