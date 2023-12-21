a6f2dcfe60868d9b5119039e7048365b1ef860ce
isaackhlam <97761927+isaackhlam@users.noreply.github.com>
2023-12-22 01:08:53 +0800

    feat: add lambda to update dynamodb for tracker real time location and package location

--- js-tracker-updater/src/index.js
+++ js-tracker-updater/src/index.js
@@ -1,4 +1,103 @@
-exports.handler = async ({ body }) => {
-  console.log(body);
-  return {statusCode: 200, body: {message: 'OK!'}};
+import { DynamoDBClient, PutItemCommand, QueryCommand } from '@aws-sdk/client-dynamodb';
+
+const TRACKER_LOCATION_TABLE = process.env.TRACKER_LOCATION_TABLE;
+const PACKAGE_TABLE = process.env.PACKAGE_TABLE;
+
+const dynamoDBClient = new DynamoDBClient({});
+
+const updatePackage = async ({
+  trackerId = "Unregister",
+  trackerType = "Truck",
+  rfid,
+}) => {
+  const queryCommand = new QueryCommand({
+    TableName: TRACKER_LOCATION_TABLE,
+    KeyConditionExpression: "id = :id",
+    ExpressionAttributeValues: {
+      ":id": { S: trackerId },
+    },
+    ScanIndexForward: false,
+    Limit: 1
+  });
+
+  let response = await dynamoDBClient.send(queryCommand);
+
+  const {
+    time,
+    latitude,
+    longitude,
+  } = response.Items[0];
+
+  const putCommand = new PutItemCommand({
+    TableName: PACKAGE_TABLE,
+    Item: {
+      packageId: {
+        S: rfid
+      },
+      locationId: {
+        S: trackerId
+      },
+      locationType: {
+        S: trackerType
+      },
+      lastUpdateTime: {
+        S: time.S
+      },
+      lastUpdateLatitude: {
+        S: latitude.S
+      },
+      lastUpdateLongitude: {
+        S: longitude.S
+      },
+    },
+  });
+  response = await dynamoDBClient.send(putCommand);
+
+  return {
+    statusCode: 200,
+    body: {response}
+  };
+}
+
+const updateTrackerLocation = async ({
+  trackerId = "Unregister",
+  time,
+  latitude,
+  longitude,
+}) => {
+  console.log(trackerId, time, latitude, longitude)
+  const command = new PutItemCommand({
+    TableName: TRACKER_LOCATION_TABLE,
+    Item: {
+      id: {
+        S: trackerId,
+      },
+      time: {
+        S: time,
+      },
+      latitude: {
+        S: String(latitude),
+      },
+      longitude: {
+        S: String(longitude),
+      },
+    },
+  });
+  const response = await dynamoDBClient.send(command);
+  return {
+    statusCode: 200,
+    body: {response}
+  };
+}
+
+export const handler = async ({body}) => {
+  const request = JSON.parse(body);
+  console.log("Request Body: ", request);
+  let resp;
+  if (request.topic === "gps")
+    resp = updateTrackerLocation(JSON.parse(request.message));
+  else
+    resp = updatePackage(JSON.parse(request.message));
+  console.log("Response: ", resp);
+  return resp;
 }
