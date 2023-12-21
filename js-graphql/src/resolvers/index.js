40062dd152075a187f2e840c954e56653529507d
isaackhlam <97761927+isaackhlam@users.noreply.github.com>
2023-12-22 02:23:46 +0800

    feat: add findPackage Resolver

--- js-graphql/src/resolvers/index.js
+++ js-graphql/src/resolvers/index.js
@@ -2,7 +2,8 @@ import merge from 'lodash/merge.js';
 
 import * as healthcheck from './healthcheck.js';
 import * as user from './user.js';
+import * as parcel from './parcel.js';
 
-const resolvers = merge({}, ...[healthcheck], ...[user]);
+const resolvers = merge({}, ...[healthcheck], ...[user], ...[parcel]);
 
 export default resolvers;
---
+++ js-graphql/src/resolvers/parcel.js
@@ -0,0 +1,30 @@
+import * as dotenv from 'dotenv';
+import { DynamoDBClient, GetItemCommand } from "@aws-sdk/client-dynamodb";
+
+dotenv.config();
+
+const PACKAGE_TABLE = process.env.PACKAGE_TABLE;
+
+const dynamoDBClient = new DynamoDBClient({});
+
+const findPackageResolver = async (_p, { input }) => {
+  const { id } = input;
+  const command = new GetItemCommand({
+    TableName: PACKAGE_TABLE,
+    Key: {
+      packageId: {
+        S: id,
+      },
+    },
+  });
+  const response = await dynamoDBClient.send(command);
+  const geoLocation = {
+    longitude: response.Item.lastUpdateLongitude.S,
+    latitude: response.Item.lastUpdateLatitude.S,
+  };
+  return {...geoLocation};
+}
+
+export const Query = {
+  findPackage: findPackageResolver
+};
